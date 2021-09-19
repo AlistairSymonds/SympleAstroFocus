0a0201b957ecd7167dd1ae8a6f2f55e0ec58a90c
AlistairSymonds <alistair.symonds@gmail.com>
2021-09-19 18:08:03 +1000

    Add TMC2209 files from tmc-api

---
+++ stm32/SympleAstroFocusStepper/tmc/ic/TMC2209/TMC2209.c
@@ -0,0 +1,190 @@
+/*
+ * TMC2209.c
+ *
+ *  Created on: 18.01.2019
+ *      Author: LK
+ */
+
+#include "TMC2209.h"
+
+// => UART wrapper
+extern void tmc2209_readWriteArray(uint8_t channel, uint8_t *data, size_t writeLength, size_t readLength);
+// <= UART wrapper
+
+// => CRC wrapper
+extern uint8_t tmc2209_CRC8(uint8_t *data, size_t length);
+// <= CRC wrapper
+
+void tmc2209_writeInt(TMC2209TypeDef *tmc2209, uint8_t address, int32_t value)
+{
+	uint8_t data[8];
+
+	data[0] = 0x05;
+	data[1] = tmc2209->slaveAddress;
+	data[2] = address | TMC_WRITE_BIT;
+	data[3] = (value >> 24) & 0xFF;
+	data[4] = (value >> 16) & 0xFF;
+	data[5] = (value >> 8 ) & 0xFF;
+	data[6] = (value      ) & 0xFF;
+	data[7] = tmc2209_CRC8(data, 7);
+
+	tmc2209_readWriteArray(tmc2209->config->channel, &data[0], 8, 0);
+
+	// Write to the shadow register and mark the register dirty
+	address = TMC_ADDRESS(address);
+	tmc2209->config->shadowRegister[address] = value;
+	tmc2209->registerAccess[address] |= TMC_ACCESS_DIRTY;
+}
+
+int32_t tmc2209_readInt(TMC2209TypeDef *tmc2209, uint8_t address)
+{
+	uint8_t data[8] = { 0 };
+
+	address = TMC_ADDRESS(address);
+
+	if (!TMC_IS_READABLE(tmc2209->registerAccess[address]))
+		return tmc2209->config->shadowRegister[address];
+
+	data[0] = 0x05;
+	data[1] = tmc2209->slaveAddress;
+	data[2] = address;
+	data[3] = tmc2209_CRC8(data, 3);
+
+	tmc2209_readWriteArray(tmc2209->config->channel, data, 4, 8);
+
+	// Byte 0: Sync nibble correct?
+	if (data[0] != 0x05)
+		return 0;
+
+	// Byte 1: Master address correct?
+	if (data[1] != 0xFF)
+		return 0;
+
+	// Byte 2: Address correct?
+	if (data[2] != address)
+		return 0;
+
+	// Byte 7: CRC correct?
+	if (data[7] != tmc2209_CRC8(data, 7))
+		return 0;
+
+	return (data[3] << 24) | (data[4] << 16) | (data[5] << 8) | data[6];
+}
+
+void tmc2209_init(TMC2209TypeDef *tmc2209, uint8_t channel, uint8_t slaveAddress, ConfigurationTypeDef *tmc2209_config, const int32_t *registerResetState)
+{
+	tmc2209->slaveAddress = slaveAddress;
+
+	tmc2209->config               = tmc2209_config;
+	tmc2209->config->callback     = NULL;
+	tmc2209->config->channel      = channel;
+	tmc2209->config->configIndex  = 0;
+	tmc2209->config->state        = CONFIG_READY;
+
+	for(size_t i = 0; i < TMC2209_REGISTER_COUNT; i++)
+	{
+		tmc2209->registerAccess[i]      = tmc2209_defaultRegisterAccess[i];
+		tmc2209->registerResetState[i]  = registerResetState[i];
+	}
+}
+
+static void writeConfiguration(TMC2209TypeDef *tmc2209)
+{
+	uint8_t *ptr = &tmc2209->config->configIndex;
+	const int32_t *settings;
+
+	if(tmc2209->config->state == CONFIG_RESTORE)
+	{
+		settings = tmc2209->config->shadowRegister;
+		// Find the next restorable register
+		while((*ptr < TMC2209_REGISTER_COUNT) && !TMC_IS_RESTORABLE(tmc2209->registerAccess[*ptr]))
+		{
+			(*ptr)++;
+		}
+	}
+	else
+	{
+		settings = tmc2209->registerResetState;
+		// Find the next resettable register
+		while((*ptr < TMC2209_REGISTER_COUNT) && !TMC_IS_RESETTABLE(tmc2209->registerAccess[*ptr]))
+		{
+			(*ptr)++;
+		}
+	}
+
+	if(*ptr < TMC2209_REGISTER_COUNT)
+	{
+		tmc2209_writeInt(tmc2209, *ptr, settings[*ptr]);
+		(*ptr)++;
+	}
+	else // Finished configuration
+	{
+		if(tmc2209->config->callback)
+		{
+			((tmc2209_callback)tmc2209->config->callback)(tmc2209, tmc2209->config->state);
+		}
+
+		tmc2209->config->state = CONFIG_READY;
+	}
+}
+
+void tmc2209_periodicJob(TMC2209TypeDef *tmc2209, uint32_t tick)
+{
+	UNUSED(tick);
+
+	if(tmc2209->config->state != CONFIG_READY)
+	{
+		writeConfiguration(tmc2209);
+		return;
+	}
+}
+
+void tmc2209_setRegisterResetState(TMC2209TypeDef *tmc2209, const int32_t *resetState)
+{
+	for(size_t i = 0; i < TMC2209_REGISTER_COUNT; i++)
+		tmc2209->registerResetState[i] = resetState[i];
+}
+
+void tmc2209_setCallback(TMC2209TypeDef *tmc2209, tmc2209_callback callback)
+{
+	tmc2209->config->callback = (tmc_callback_config) callback;
+}
+
+uint8_t tmc2209_reset(TMC2209TypeDef *tmc2209)
+{
+	if(tmc2209->config->state != CONFIG_READY)
+		return false;
+
+	// Reset the dirty bits and wipe the shadow registers
+	for(size_t i = 0; i < TMC2209_REGISTER_COUNT; i++)
+	{
+		tmc2209->registerAccess[i] &= ~TMC_ACCESS_DIRTY;
+		tmc2209->config->shadowRegister[i] = 0;
+	}
+
+	tmc2209->config->state        = CONFIG_RESET;
+	tmc2209->config->configIndex  = 0;
+
+	return true;
+}
+
+uint8_t tmc2209_restore(TMC2209TypeDef *tmc2209)
+{
+	if(tmc2209->config->state != CONFIG_READY)
+		return false;
+
+	tmc2209->config->state        = CONFIG_RESTORE;
+	tmc2209->config->configIndex  = 0;
+
+	return true;
+}
+
+uint8_t tmc2209_get_slave(TMC2209TypeDef *tmc2209)
+{
+	return tmc2209->slaveAddress;
+}
+
+void tmc2209_set_slave(TMC2209TypeDef *tmc2209, uint8_t slaveAddress)
+{
+	tmc2209->slaveAddress = slaveAddress;
+}
---
+++ stm32/SympleAstroFocusStepper/tmc/ic/TMC2209/TMC2209.h
@@ -0,0 +1,96 @@
+/*
+ * TMC2209.h
+ *
+ *  Created on: 18.01.2019
+ *      Author: LK
+ */
+
+#ifndef TMC_IC_TMC2209_H_
+#define TMC_IC_TMC2209_H_
+
+#include "tmc/helpers/Constants.h"
+#include "tmc/helpers/API_Header.h"
+#include "TMC2209_Register.h"
+#include "TMC2209_Constants.h"
+#include "TMC2209_Fields.h"
+
+// Helper macros
+#define TMC2209_FIELD_READ(tdef, address, mask, shift) \
+	FIELD_GET(tmc2209_readInt(tdef, address), mask, shift)
+#define TMC2209_FIELD_UPDATE(tdef, address, mask, shift, value) \
+	(tmc2209_writeInt(tdef, address, FIELD_SET(tmc2209_readInt(tdef, address), mask, shift, value)))
+
+// Usage note: use 1 TypeDef per IC
+typedef struct {
+	ConfigurationTypeDef *config;
+
+	int32_t registerResetState[TMC2209_REGISTER_COUNT];
+	uint8_t registerAccess[TMC2209_REGISTER_COUNT];
+
+	uint8_t slaveAddress;
+} TMC2209TypeDef;
+
+typedef void (*tmc2209_callback)(TMC2209TypeDef*, ConfigState);
+
+// Default Register values
+#define R00 0x00000040  // GCONF
+#define R10 0x00071703  // IHOLD_IRUN
+#define R11 0x00000014  // TPOWERDOWN
+#define R6C 0x10000053  // CHOPCONF
+#define R70 0xC10D0024  // PWMCONF
+
+// Register access permissions:
+//   0x00: none (reserved)
+//   0x01: read
+//   0x02: write
+//   0x03: read/write
+//   0x13: read/write, seperate functions/values for reading or writing
+//   0x23: read/write, flag register (write to clear)
+//   0x42: write, has hardware presets on reset
+static const uint8_t tmc2209_defaultRegisterAccess[TMC2209_REGISTER_COUNT] =
+{
+//  0     1     2     3     4     5     6     7     8     9     A     B     C     D     E     F
+	0x03, 0x23, 0x01, 0x02, 0x02, 0x01, 0x01, 0x03, ____, ____, ____, ____, ____, ____, ____, ____, // 0x00 - 0x0F
+	0x02, 0x02, 0x01, 0x02, 0x02, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, // 0x10 - 0x1F
+	____, ____, 0x02, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, // 0x20 - 0x2F
+	____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, // 0x30 - 0x3F
+	0x02, 0x01, 0x02, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, // 0x40 - 0x4F
+	____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, // 0x50 - 0x5F
+	____, ____, ____, ____, ____, ____, ____, ____, ____, ____, 0x01, 0x01, 0x03, ____, ____, 0x01, // 0x60 - 0x6F
+	0x03, 0x01, 0x01, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____  // 0x70 - 0x7F
+};
+
+static const int32_t tmc2209_defaultRegisterResetState[TMC2209_REGISTER_COUNT] =
+{
+//	0    1    2    3    4    5    6    7    8    9    A    B    C    D    E    F
+	R00, 0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, // 0x00 - 0x0F
+	R10, R11, 0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, // 0x10 - 0x1F
+	0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, // 0x20 - 0x2F
+	0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, // 0x30 - 0x3F
+	0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, // 0x40 - 0x4F
+	0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, // 0x50 - 0x5F
+	0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   R6C, 0,   0,   0, // 0x60 - 0x6F
+	R70, 0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0  // 0x70 - 0x7F
+};
+
+#undef R00
+#undef R10
+#undef R11
+#undef R6C
+#undef R70
+
+// Communication
+void tmc2209_writeInt(TMC2209TypeDef *tmc2209, uint8_t address, int32_t value);
+int32_t tmc2209_readInt(TMC2209TypeDef *tmc2209, uint8_t address);
+
+void tmc2209_init(TMC2209TypeDef *tmc2209, uint8_t channel, uint8_t slaveAddress, ConfigurationTypeDef *tmc2209_config, const int32_t *registerResetState);
+uint8_t tmc2209_reset(TMC2209TypeDef *tmc2209);
+uint8_t tmc2209_restore(TMC2209TypeDef *tmc2209);
+void tmc2209_setRegisterResetState(TMC2209TypeDef *tmc2209, const int32_t *resetState);
+void tmc2209_setCallback(TMC2209TypeDef *tmc2209, tmc2209_callback callback);
+void tmc2209_periodicJob(TMC2209TypeDef *tmc2209, uint32_t tick);
+
+uint8_t tmc2209_get_slave(TMC2209TypeDef *tmc2209);
+void tmc2209_set_slave(TMC2209TypeDef *tmc2209, uint8_t slaveAddress);
+
+#endif /* TMC_IC_TMC2209_H_ */
