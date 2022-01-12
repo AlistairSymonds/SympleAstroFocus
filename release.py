ca619e392f1641797dffbfcbda0feaf12182ab0c
AlistairSymonds <alistair.symonds@gmail.com>
2022-01-13 00:09:09 +1100

    Sudo code release script

---
+++ release.py
@@ -0,0 +1,19 @@
+#argparse paths to executables
+
+#validate change notes/version history
+
+#embed relevant git to stamp files
+
+#clean STM output
+
+#compile STM32 code in release mode (some -D flag that ensures some proper stamping of the bin?)
+
+#clean VS output
+
+#compile dll
+
+#compile test app
+
+#package dll into ISS installer
+
+#done!
